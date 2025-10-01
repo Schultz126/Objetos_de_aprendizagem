e30f5d2c2e06bccc5051cda4fc0c536f615f9527
Schultz126 <rafael.drzevieski@gmail.com>
2025-10-01 20:45:13 -0300

    Vetor sendo criado
    Os elementos de cada caixa são gerados assim que a página é carregada

--- busca_linear/js/arrayGenerator.js
+++ busca_linear/js/arrayGenerator.js
@@ -1,3 +1,10 @@
+document.querySelector('button#generateNumbersButton').addEventListener('click', generateArray);
+window.addEventListener('load', generateArray);
+
 function generateArray() {
-    
+    let button = document.getElementsByClassName('value');      
+    for(let i = 0; i < 10; i++) {
+        button[i].textContent = Math.floor(Math.random() * 50 + 1);
+    }
 }
+
