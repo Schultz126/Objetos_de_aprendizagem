9a858e58637d5f57077278a3cf966bdf2fba9f93
Schultz126 <rafael.drzevieski@gmail.com>
2025-10-02 11:26:45 -0300

    Reutilização da base do projeto de busca sequencial
    O estilo da busca sequencial foi mantido para encurtar o desenvolvimento. Arquivo gerador do vetor foi modificado para criar elementos ordenadamente

---
+++ busca_binaria/js/arrayGenerator.js
@@ -0,0 +1,16 @@
+document.querySelector('button#generateNumbersButton').addEventListener('click', generateArray);
+window.addEventListener('load', generateArray);
+
+function generateArray() {
+    let auxiliaryArray = [];
+    let button = document.getElementsByClassName('value');      
+    for(let i = 0; i < 10; i++) {
+        auxiliaryArray[i] = Math.floor(Math.random() * 99 + 1);
+    }
+    auxiliaryArray.sort(function(a, b) { return a - b; });
+    for(let i = 0; i < 10; i++) {
+        button[i].textContent = auxiliaryArray[i];
+    }
+}
+
+
