3aa24d72c6ecf21eab05ca22557e959fecb12ebf
Schultz126 <rafael.drzevieski@gmail.com>
2025-10-02 07:31:58 -0300

    Primeiro passo aplicado
    O primeiro passo da busca foi implementado, agora deve ser aplicado aos demais elementos

--- busca_linear/js/arrayGenerator.js
+++ busca_linear/js/arrayGenerator.js
@@ -4,7 +4,7 @@ window.addEventListener('load', generateArray);
 function generateArray() {
     let button = document.getElementsByClassName('value');      
     for(let i = 0; i < 10; i++) {
-        button[i].textContent = Math.floor(Math.random() * 50 + 1);
+        button[i].textContent = Math.floor(Math.random() * 99 + 1);
     }
 }
 
--- busca_linear/js/linearSearch.js
+++ busca_linear/js/linearSearch.js
@@ -1,8 +1,38 @@
+var searchedNumber;
+var array = document.getElementsByClassName('value');
+let fullValueBox = document.getElementsByClassName("number_box");
+let isSearching = false;
+let currentInfo = {current: fullValueBox[0], index: 0}
+var list = [];
+
 document.querySelector('button#submitButton').addEventListener('click', function() {
-    let searchedNumber = document.querySelector('input#searchedNumber').value;
+    searchedNumber = document.querySelector('input#searchedNumber').value;
     if(searchedNumber.trim() === "") {
         console.log("Digite um número");
     } else {
+        searchedNumber = parseInt(searchedNumber, 10);
         console.log(searchedNumber);
+        for(let i = 0; i < 8; i++) {
+            list[i] = parseInt(array[i].textContent);
+        }
+        startSearch(0);
     }
-});
+    });
+
+function startSearch(index) {
+    isSearching = true;
+    if(list[index] === searchedNumber) {
+        currentInfo.current.classList.add("correct");
+        document.querySelector("article#status").textContent = "Encontrado";
+    }
+    currentInfo.current.classList.add("current");   
+}
+
+document.querySelector('button#nextStep').addEventListener('click', function() {
+    if(!isSearching) {
+        window.alert("Busca ainda não iniciada");
+    }
+    else {
+        alert("Busca iniciada");
+    }
+})
