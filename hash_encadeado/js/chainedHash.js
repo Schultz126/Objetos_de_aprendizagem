ff1a2ca4330b1c48ea975f6cbe4bc6f4ad5d9ed9
Schultz126 <rafael.drzevieski@gmail.com>
2025-10-04 22:34:37 -0300

    Projeto finalizado
    Funciona perfeitamente. Estilos foram feitos por IA para que os demais possam ser iniciados o quanto antes

--- hash_encadeado/js/chainedHash.js
+++ hash_encadeado/js/chainedHash.js
@@ -21,14 +21,39 @@ function createElement(value) {
     let index = hash(value); // Índice em que o novo elemento será colocado
     let parent = rows[index]; // Linha da tabela em que o elemento será inserido
     let newElement = document.createElement('span'); // Criação do elemento
-    newElement.classList.add('root'); 
+    newElement.classList.add('span:not(.root)'); 
     newElement.classList.add('newElement'); 
     newElement.textContent = value;
 
     if(parent) {
         parent.appendChild(newElement);
+        visualHash(value, index);
     }
     else {
         alert("Não foi possível inserir o elemento");
     }
 }
+
+function visualHash(value, index) {
+    var textBox = document.querySelector('article#visualExplanation');
+    let message = `Resto da divisão de ${value} por 7 é ${index}, portanto este será seu índice`; // Template da mensagem que será exibida
+
+    textBox.textContent = message;
+}
+
+document.querySelector('button#resetButton').addEventListener('click', function() {
+    let inputVal = document.querySelector('input#insertedNumber');
+    let textBox = document.querySelector('article#visualExplanation');
+    const elementLists = document.querySelectorAll('.elementList');
+        elementLists.forEach(list => {
+            // Encontra todos os elementos que não são a raiz
+            const elementsToRemove = list.querySelectorAll('span:not(.root)');
+            elementsToRemove.forEach(element => {
+                // Remove o elemento do DOM
+                element.remove();
+                });
+            }); 
+    // Limpa o campo do input de números e remove a mensagem
+    inputVal.value = '';
+    textBox.value = '';
+})
